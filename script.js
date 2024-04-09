5ef72d043e8b0176782eaf4e08d7044b653431b7
jacksmolenski <jt02smol@siena.edu>
2024-04-09 14:32:00 -0400

    Worked on files

--- script.js
+++ script.js
@@ -15,4 +15,22 @@ window.onscroll = function() {
     document.body.scrollTop = 0; 
     document.documentElement.scrollTop = 0; 
   }
+
+  let slideIndex = 0;
+  const slides = document.querySelectorAll('.slideshow-item');
+  
+  function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    setTimeout(showSlides, 8000); // Change image every 2 seconds
+  }
+  
+  showSlides();
+  
   
